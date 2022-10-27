dcf23d7dad9f4ad8bd551eac4702fc3dc1efe5fe
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-10-26 22:08:56 -0700

    Complete exercise 5 and extra credit 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -39,7 +39,7 @@ function AppProvider({children}) {
     grid: initialGrid,
   })
   // 🐨 memoize this value with React.useMemo
-  const value = [state, dispatch]
+  const value = React.useMemo(() => [state, dispatch], [state])
   return (
     <AppStateContext.Provider value={value}>
       {children}
@@ -52,11 +52,19 @@ function useAppState() {
   if (!context) {
     throw new Error('useAppState must be used within the AppProvider')
   }
-  return context
+  return context[0]
+}
+
+function useAppDispatch() {
+	const context = React.useContext(AppStateContext)
+  if (!context) {
+    throw new Error('useAppState must be used within the AppProvider')
+  }
+  return context[1]
 }
 
 function Grid() {
-  const [, dispatch] = useAppState()
+  const dispatch = useAppDispatch()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
   const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
@@ -74,7 +82,8 @@ function Grid() {
 Grid = React.memo(Grid)
 
 function Cell({row, column}) {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+	const dispatch = useAppDispatch();
   const cell = state.grid[row][column]
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -93,7 +102,8 @@ function Cell({row, column}) {
 Cell = React.memo(Cell)
 
 function DogNameInput() {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+	const dispatch = useAppDispatch();
   const {dogName} = state
 
   function handleChange(event) {
